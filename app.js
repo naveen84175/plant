9be3798e15764c8d83fa64587c9f8a5d3493cd4e
Ronit <70330163+asawaronit60@users.noreply.github.com>
2022-01-05 02:23:03 +0530

    db asscess way changed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ dotenv.config({path:'./config.env'})
 const app = express();
 
 
-const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.PASSWORD)
+const DB = 'mongodb+srv://plantifier2020:<PASSWORD>@cluster0.casik.mongodb.net/plantifier?retryWrites=true'.replace('<PASSWORD>',process.env.PASSWORD)
 mongoose.connect(DB,
     {useNewUrlParser: true}).then(()=>{
     console.log('Connected to the database Successfully')
