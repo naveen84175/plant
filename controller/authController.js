e698433f43735900cce1ff712eefbad8812400ab
Ronit <70330163+asawaronit60@users.noreply.github.com>
2022-06-10 08:19:38 +0530

    jwt exp changed

--- controller/authController.js
+++ controller/authController.js
@@ -8,7 +8,7 @@ const email = require('../utils/email')
 
 const SignupToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: '2h',
   });
 };
 
